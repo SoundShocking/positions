ba7abd73b6036d018c87254e6c1aea704fda462e
SoundShocking <nireemk@gmail.com>
2021-08-28 08:47:47 +0300

    27.08

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 import 'simplebar'
+
+const contactFormFields = document.querySelectorAll('.contact-form__form .contact-form__input, .contact-form__form .contact-form__textarea')
+if (contactFormFields.length) {
+  contactFormFields.forEach(el => {
+    el.value = '';
+    el.addEventListener('input', e => {
+      const nextSibling = e.target.nextElementSibling;
+
+      if (e.target.value.length > 0) {
+        if (nextSibling) {
+          nextSibling.style.display = 'none';
+        }
+      } else {
+        if (nextSibling) {
+          nextSibling.style.display = 'block';
+        }
+      }
+    })
+  })
+}
